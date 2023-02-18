a4c3de7ad58ae2bf63b1e2e452092c6529f8f8c9
Giuseppe Setem <giuseppe7d@gmail.com>
2023-02-18 20:32:20 -0300

    feat: add build stage to api

--- apps/api/src/serverless.ts
+++ apps/api/src/serverless.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-import app from './src/app'
+import app from '@/app'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   await app.ready()
