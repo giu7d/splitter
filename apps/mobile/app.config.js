71b8915d00eaf3c62601e18d6aa02fb9de78699f
Giuseppe Setem <giuseppe7d@gmail.com>
2023-02-19 21:19:07 -0300

    chore: change runtime version

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -17,7 +17,7 @@ export default {
       fallbackToCacheTimeout: 0,
       url: 'https://u.expo.dev/916828f4-940a-42fa-a42d-a662c47baf27'
     },
-    runtimeVersion: '1.0.0',
+    runtimeVersion: '47.0.0',
     assetBundlePatterns: ['**/*'],
     plugins: ['expo-community-flipper'],
     ios: {
