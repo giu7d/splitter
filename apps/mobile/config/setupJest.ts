938dc6944bd96d053abc3702d9d24b8307a36843
Giuseppe Setem <giuseppe7d@gmail.com>
2023-05-12 12:13:25 -0300

    test: mock reanimated data mock

--- apps/mobile/config/setupJest.ts
+++ apps/mobile/config/setupJest.ts
@@ -5,8 +5,20 @@ jest.setTimeout(30000)
 
 SetupJestFetch.enableMocks()
 
+// @ts-expect-error
+global.window = {}
+
+// @ts-expect-error
+global.window = global
+
 beforeAll(() => {
+  // @ts-expect-error
+  global.ReanimatedDataMock = {
+    now: jest.fn()
+  }
+
   jest.spyOn(console, 'error').mockImplementation(() => {})
+
   server.listen()
 })
 
