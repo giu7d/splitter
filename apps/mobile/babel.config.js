aca393c6f8601e04592dea7694b3c5d245920b5d
Giuseppe Setem <giuseppe7d@gmail.com>
2023-06-29 09:07:05 -0300

    feat: add expo router v1 configurations

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      require.resolve('expo-router/babel'),
       'nativewind/babel',
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
