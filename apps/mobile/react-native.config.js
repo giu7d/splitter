0dd6ac3fa66fabcf48e2d4cbb3946d60742c7a6d
Giuseppe Setem <giuseppe7d@gmail.com>
2023-07-10 13:40:23 -0300

    feat: handle deprecated modules

---
+++ apps/mobile/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {
+      unstable_reactLegacyComponentNames: [
+        'react-native-fast-image',
+        'CellContainer',
+        'AutoLayoutView'
+      ]
+    },
+    android: {}
+  }
+}
