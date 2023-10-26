af7b4dfa32f081fca4cb9194941ba265f007c762
Giuseppe Setem <giuseppe7d@gmail.com>
2023-10-26 20:46:04 -0300

    feat(core): add poc for activating live activity

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import { View } from 'react-native'
 
 import CreateBill from '@/components/container/Bills/CreateBill'
 import FilterBills from '@/components/container/Bills/FilterBills'
 import ListBills from '@/components/container/Bills/ListBills'
+import Button from '@/components/fragments/Button'
 import BaseTemplate from '@/components/templates/Base'
 import DrawerTemplate from '@/components/templates/Drawer'
 import useDrawer from '@/hooks/useDrawer'
 import { trpc } from '@/services/api'
+import SplitLiveActivity from '@/services/notifications/splitLiveActivity'
 
 export default function App() {
   const drawer = useDrawer()
+  // TODO: REMOVE THIS
+  const [randomId, setRandomId] = useState<string>('')
+  // TODO: REMOVE THIS
+  const [progress, setProgress] = useState<number>(0)
 
   const user = trpc.users.find.useQuery({ id: 'my-id' })
   const bills = trpc.bills.list.useQuery()
@@ -40,6 +47,68 @@ export default function App() {
         >
           <View className="flex-grow h-full pb-20">
             <ListBills />
+            {/* TODO: REMOVE THIS */}
+            {!randomId && (
+              <Button.Primary
+                className="m-4"
+                onPress={() =>
+                  SplitLiveActivity.start({
+                    title: 'Churras',
+                    subtitle: 'Aguardando a galera...',
+                    icon: '🍖',
+                    progress
+                  })
+                }
+              >
+                Create Activity
+              </Button.Primary>
+            )}
+            {!randomId && (
+              <Button.Primary
+                className="m-4"
+                onPress={async () => {
+                  const [activity] = await SplitLiveActivity.listAll()
+
+                  if (!activity) return
+
+                  setRandomId(activity.id)
+                }}
+              >
+                Load Id Activity
+              </Button.Primary>
+            )}
+            {randomId && (
+              <Button.Primary
+                className="m-4"
+                onPress={() => {
+                  setProgress(1)
+
+                  console.log({ randomId, progress })
+
+                  SplitLiveActivity.update(randomId, {
+                    title: 'Churras',
+                    subtitle: 'Aguardando a galera...',
+                    icon: '🍖',
+                    progress: 1
+                  })
+                }}
+              >
+                Update Activity
+              </Button.Primary>
+            )}
+            {randomId && (
+              <Button.Primary
+                className="m-4"
+                onPress={() => {
+                  SplitLiveActivity.end(randomId)
+                  setRandomId('')
+                  setProgress(0)
+                }}
+              >
+                End Activity
+              </Button.Primary>
+            )}
+            {/* TODO: REMOVE THIS */}
           </View>
         </BaseTemplate.Root>
       </DrawerTemplate>
